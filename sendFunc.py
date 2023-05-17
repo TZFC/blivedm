2a2358e9e860e22e0d327b77454f925363335886
TZFC <chenfangchen2000@gmail.com>
2023-05-16 20:27:24 -0400

    update credentials before send

--- sendFunc.py
+++ sendFunc.py
@@ -1,5 +1,6 @@
-import sendUtil
+from sendUtil import updateCredentials, sendText
 
 
 async def renqiRemind(userConfig, streamInfo):
-    sendUtil.sendText(text="送送人气票喵~右上角有免费的人气票喵~", room=userConfig["ROOM_ID"])
+    updateCredentials()
+    sendText(text="送送人气票喵~右上角有免费的人气票喵~", room=userConfig["ROOM_ID"])
