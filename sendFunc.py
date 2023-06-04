1bcce3c8119300e9bd90d7ad5876dd2ad11d8c30
TZFC <chenfangchen2000@gmail.com>
2023-06-03 20:38:33 -0400

    add handling of invalid title

--- sendFunc.py
+++ sendFunc.py
@@ -18,6 +18,9 @@ async def luboComment(userConfig, streamInfo):
     for repo in userConfig["LUBO_API"]:
         latest = requests.get(repo).json()["data"]['archives'][0]
         filename = translateTitle(latest["title"])
+        if filename == "invalid title":
+            streamInfo["last_comment_aids"][repo] = latest["aid"]
+            continue
         if filename < lesser_file:
             lesser_file = filename
         if latest["aid"] == streamInfo["last_comment_aids"][repo]:
@@ -32,7 +35,7 @@ async def luboComment(userConfig, streamInfo):
             except:
                 with open("exception.txt", "a", encoding="utf-8") as log:
                     log.write("{} not found for {}\n".format(filename, latest["title"]))
-            streamInfo["last_comment_aids"][repo] = latest["aid"]
+        streamInfo["last_comment_aids"][repo] = latest["aid"]
     if lesser_file == "9": # in case api returns unexpected response
         return
     for file in os.listdir("danmu/{}".format(userConfig["ROOM_NAME"])):
@@ -43,6 +46,10 @@ async def luboComment(userConfig, streamInfo):
 def translateTitle(luboTitle) -> str:
     # expect lubo Title to have 非数字内容<year>年<month>月<date>日<hour>非数字内容 as the last 4 separated integer segments
     pattern = r'[^0-9]*([0-9]+)年([0-9]+)月([0-9]+)日([0-9]+)[^0-9]*'
-    segments = re.findall(pattern, luboTitle)[0]
+    found = re.findall(pattern, luboTitle)
+    if len(found)==1:
+        segments = found[0]
+    else:
+        return "invalid title"
     filled_segments = [x if len(x) > 1 else "0" + x for x in segments]
     return "-".join(filled_segments) + ".txt"
